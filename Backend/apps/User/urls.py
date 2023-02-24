020872bf435d2297660995ebe51ec64e48008342
Quilla <101010846+sirawitchair@users.noreply.github.com>
2023-02-24 15:53:49 +0700

    create update

--- Backend/apps/User/urls.py
+++ Backend/apps/User/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from .viewsets import UserViewSet
-
+from .views import createUser,updateUser
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -10,7 +10,11 @@ router.register('User', UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
+app_name = 'User'
 urlpatterns = [
     path('api/', include(router.urls)),
+    
+    path('create_user/',createUser,name="create_user"),
+    path('update_user/<user_id>',updateUser,name="update_user")
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- Backend/apps/User/views.py
+++ Backend/apps/User/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,HttpResponseForbidden
+
+from .models import User
+from .forms import UserForm
 
 
+def createUser(request):
+    
+    form = UserForm()
+    if request.method=='POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context={'form':form}
+    return render(request,'User/register.html',context)
+
+def updateUser(request,user_id):
+    
+    user_update=User.objects.get(id=user_id)
+    if user_update.document_approved:
+        return HttpResponseForbidden()
+    if user_update.data_user!=request.user:
+        return HttpResponseForbidden()
+    form = UserForm(instance=user_update)
+    if request.method=='POST':
+        form = UserForm(request.POST,instance=user_update)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    
+    context={'form':form}
+    return render(request,'User/register.html',context)
