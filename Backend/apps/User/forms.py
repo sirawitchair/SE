c5a7de9e9086810ed38f2fbc1f667fce710ef644
Quilla <101010846+sirawitchair@users.noreply.github.com>
2023-03-01 19:19:11 +0700

    regisform

--- Backend/apps/User/forms.py
+++ Backend/apps/User/forms.py
@@ -1,7 +1,29 @@
 from django.forms import ModelForm
 from .models import User
+from django import forms 
+
+CHOICE_GENDER=(
+    (0,"Male"),
+    (1,"Female"),
+    (2,"Other")
+)
 
 class UserForm(ModelForm) :
     class Meta:
         model = User
         fields = '__all__'
+        
+        widgets = {
+            'first_name' : forms.TextInput(attrs={'placeholder': 'First Name','class': 'form-control form-control-lg'}),
+            'last_name' : forms.TextInput(attrs={'placeholder': 'Last Name','class': 'form-control form-control-lg'}),
+            'birth_day' : forms.DateInput(format=('%Y-%m-%d'),attrs={'type': 'date'}),
+            'person_id' : forms.TextInput(attrs={'placeholder': 'Person ID','class': 'form-control form-control-lg'}),
+            'password' : forms.TextInput(attrs={'placeholder': 'Password','class': 'form-control form-control-lg'}),
+            'mobile' : forms.TextInput(attrs={'placeholder': 'Phone Number','class': 'form-control form-control-lg'}),
+            'email' : forms.TextInput(attrs={'placeholder': 'Email','class': 'form-control form-control-lg','type':'email'}),            
+            'address' : forms.TextInput(attrs={'class':'form-control form-control-lg'}),
+            # 'sex':forms.Select(attrs={'class':'d-md-flex justify-content-start align-items-center mb-4 py-2','type':'radio'}),
+            # 'sex':forms.CheckboxInput(attrs={'class':'d-md-flex justify-content-start align-items-center mb-4 py-2','type':'radio'},choices=0),      
+            # 'sex':forms.CheckboxInput(attrs={'class':'d-md-flex justify-content-start align-items-center mb-4 py-2','type':'radio'}),   
+            'sex':forms.Select(attrs={'class':'col-md-1.5 mb-4',},)
+        }
